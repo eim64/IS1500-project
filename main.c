7f1e76367d7a403c71a88dab9b93a5ad620499b6
Emil <eim.wco@gmail.com>
2021-11-15 16:23:23 +0100

    prepare project base

---
+++ main.c
@@ -0,0 +1,62 @@
+/* main.c
+
+   This file written 2015 by F Lundevall and David Broman
+
+   Latest update 2015-09-15 by David Broman
+
+   For copyright and licensing, see file COPYING */
+
+#include <stddef.h>   /* Declarations of integer sizes and the like, part 1 */
+#include <stdint.h>   /* Declarations of integer sizes and the like, part 2 */
+#include <pic32mx.h>  /* Declarations of system-specific addresses etc */
+void user_isr() { }
+extern void game_logic();
+
+int main()
+{
+
+  // Prepare Uno32
+  {
+    //OSCCONCLR = 0x100000; /* clear PBDIV bit 1 */
+    //OSCCONSET = 0x080000; /* set PBDIV bit 0 */
+    
+    /* Set up output pins */
+    AD1PCFG = 0xFFFF;
+    ODCE = 0x0;
+    TRISECLR = 0xFF;
+    PORTE = 0x0;
+    
+    /* Output pins for display signals */
+    PORTF = 0xFFFF;
+    PORTG = (1 << 9);
+    ODCF = 0x0;
+    ODCG = 0x0;
+    TRISFCLR = 0x70;
+    TRISGCLR = 0x200;
+    
+    /* Set up input pins */
+    TRISDSET = (1 << 8);
+    TRISFSET = (1 << 1);
+    
+    /* Set up SPI as master */
+    SPI2CON = 0;
+    SPI2BRG = 4;
+    /* SPI2STAT bit SPIROV = 0; */
+    SPI2STATCLR = 0x40;
+    /* SPI2CON bit CKP = 1; */
+    SPI2CONSET = 0x40;
+    /* SPI2CON bit MSTEN = 1; */
+    SPI2CONSET = 0x20;
+    /* SPI2CON bit ON = 1; */
+    SPI2CONSET = 0x8000;
+  }
+
+
+  TRISE &= ~0xFF;
+  TRISD |= 0xE0;
+  TRISF |= 0x1;
+
+  while(1)
+  {
+  }
+}
